e1e0aa4b580cdf334d3ec78aba02b847265dd678
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-08-21 18:44:24 -0700

    Simplifies email regex

--- src/models/Auth.js
+++ src/models/Auth.js
@@ -32,7 +32,7 @@ export type ValidationResult =
   | {| +error: FindiError, +type: 'NOT_VALID' |};
 
 // eslint-disable-next-line
-const EMAIL_REGEXP = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
+// const EMAIL_REGEXP = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
 
 /**
  * Login a user with the given email and password.
@@ -56,7 +56,9 @@ export function genLogout(): Promise<void> {
 }
 
 export function validateSignUpForm(signUpForm: SignUpForm): ValidationResult {
-  if (!EMAIL_REGEXP.test(signUpForm.email)) {
+  // TODO: Having some wierd issues with the email regex above, trying something
+  // stupidly simple for now.
+  if (!signUpForm.email.includes('@')) {
     return notValidWithError(
       FindiError.fromRaw({
         errorCode: 'CORE / VALIDATION_ERROR',
